a0b0d690e02d9d5dd431c8a9735957ef732bb6cb
Aakash Bhandari <akashbhandari930@gmail.com>
2021-11-24 18:48:39 -0800

    updated the code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# DayPlanner
+# 05 Third-Party APIs: Work Day Scheduler
+
+# Summary
+*This application is a simple calander that allows the user to save events for each hour of the day and As an employee with a busy schedule we want to add important events to a daily planner, So that we can manage my time effectively*. 
+
+# Screenshot
+![DayPlanner](asset/images/WorkDayScheduler.gif)
+
+# Git Hub:
+
+*My project was pushed to GitHub at the following repository*:https://github.com/syke2good/portfolio
+
+*You can access the deployed application with the GitHup Pages link*:https://syke2good.github.io/portfolio/
+
+# WEB API's: 
+- jQuery - Used for event listeners of parent and childeren elements as well as to store and recall those varible in local storage.
+- momentjs - Used to pull current date and local time.
+- javascript - Used to dynamically change html and store user-input.
+- Bootstrap - Used to pull existing html and CSS for creating resposive organizational structer and styling for the site.
+- HTML - Used to create elements on the DOM
+- CSS - Styles html elements on page
+- Git - Version control system to track changes to source code
+- GitHub - Hosts repository that can be deployed to GitHub Pages
+
+# Sources: 
+- jQuery
+- HTML
+- CSS
+- Boostrap
+
+# License Copyright (c) 2012-2021 Scott Chacon and others
+
+*Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:*
+
+*The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.*
+
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*
--- asset/script.js
+++ asset/script.js
@@ -1,64 +1,45 @@
-// Wait until Jquery is ready
-$( document ).ready(function() {
-    //Display current date
-    //Variable to append cols
-  var a= moment().format('dddd MMMM Do YYYY, h:mm');
-    $("#display-date").text(a)
-    var row = ""
-    //loop to dispaly 9am-18pm
-      for (var i= 9 ; i<=18; i++){
-        // Creation of the row elements
-        row = $(`<div class="row">`)
-        col1 = $(`<div class ="col-lg-2 hour">${displayAmorPm(i)}</div>`)
-        col2 = $(`<div class ="col-lg-8 inputcontent"><input data-input="${i}" id="inputText${i}" class="form-control inputText" type="text" name="userInput"></div>`)
-        col3 = $(`<div class ="col-lg-2"><button data-id="${i}" id="savePlanner" class="btn btn-success btn-block"><i class="fas fa-save"></i> Save</button></div>`)
-        row.append(col1)
-        row.append(col2)
-        row.append(col3)
-        $("#display-planner").append(row)
-        getlocalStorage(i)
-      }
-     $("button.btn.btn-success").click(function(e){
-     var id = $(this).data("id")
-     var inputText = $(this).parent().siblings().find("input").val()
-     localStorage.setItem(id,inputText)
-     })
-    //  Convert Am to Pm
-     function displayAmorPm(hour){
-       var b=""
-       if(hour<=12){
-         b= "AM"
-       }else{
-         b="PM"
-       }
-       hour = hour % 12
-       hour = hour ? hour : 12
-       return hour + " " + b
-     }
-    });
-  
-     function getlocalStorage(hour){
-       let inputval = localStorage.getItem(hour)
-       if(true){
-        //  $("input").data(`input${hour}`)
-        var text= $(`input#inputText${hour}`).val(inputval)
-        console.log(text)
-       }
-     }
-     //Apdate color
-     function updateColor(){
-       var hour = new Date().getHours();
-       for (var i= 9 ; i<=18; i++){
-         console.log(hour,i)
-         if(hour==i ) {
-          $(`#inputText${i}`).css("background","red")
-         }else  if(hour<i ){
-          
-           $(`#inputText${i}`).css("background","lightblue")
-  
-         }
-       }
-     }
-     setInterval(function(){
-       updateColor()
-     },1000)
+$(init);
+
+function init() {
+  // get current day and display on top of page
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  // color our time blocks and start interval to re-color every minute
+  colorTimeBlocks();
+  setInterval(colorTimeBlocks, 60000);
+
+  // update time blocks with data in local storage
+  $(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    // load saved data from local storage
+    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+  // attach our handler for the save buttons
+  $(".saveBtn").on("click", handleSave);
+}
+
+function colorTimeBlocks() {
+  // for each time block
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    // remove any class we may have added before
+    $(this).removeClass("past present future");
+    // color block based on past, present, future class
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+function handleSave(event) {
+  // get the id of our parent
+  var hourId = $(this).parent().attr("id");
+  // save data in textarea in local storage
+  localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
+}
